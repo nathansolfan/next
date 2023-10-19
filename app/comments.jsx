f961940488c6d62d47fe4a4501dce2b9735d718a
nathansolfan <nathansolfan@hotmail.com>
2023-10-19 15:32:14 +0100

    started next.js - clear prelayout

---
+++ app/comments.jsx
@@ -0,0 +1,5 @@
+// Only comments // Steps:
+// 1 - Delete pre next layout
+// 2 - Change Page ad Layout to jsx
+// 3 - Delete pre CSS 
+//
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+// 1st step we delete the pre next js 
+// 2nd chage page.js and layout.js to jsx
+
+export default function Home() {
+  return (
+    <main>
+      <h2>Dashboard</h2>
+    </main>
+  );
+}
