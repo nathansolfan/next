d24b4208bd1c9b2895fb79c3febe2ffad1eb65d0
nathansolfan <nathansolfan@hotmail.com>
2023-10-20 13:57:23 +0100

    Components creation

---
+++ app/about/information.jsx
@@ -0,0 +1,12 @@
+// we create a page here and we can access that on the url
+// we can create folder insider folder
+// components are SERVER components by default
+const About = () => {
+    return (
+        <main>
+            <h2>About me</h2>
+        </main>
+    );
+}
+ 
+export default About;
