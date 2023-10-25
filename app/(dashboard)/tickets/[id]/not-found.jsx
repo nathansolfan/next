863582fea208fd4ffbc7cd0c3941097219dae647
nathansolfan <nathansolfan@hotmail.com>
2023-10-25 16:36:51 +0100

    This is a custom 404 page for specific pages

--- app/(dashboard)/tickets/[id]/not-found.jsx
+++ app/(dashboard)/tickets/[id]/not-found.jsx
@@ -6,7 +6,7 @@ export default function NotFound() {
   return (
     // text-center is a tailwind class
     <main className='text-center'>
-        <h2 className='text-3x1'>There was a problem</h2>
+        <h2 className='text-3x1'>THIS IS A SPECIFIC ERROR instead of 404</h2>
         <p>We couldnt find the page</p>
         {/* we link to dashboard inside a <p/> */}
         <p> <Link href="/">Dashboard</Link> </p>
