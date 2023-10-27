8848403d1e379e23b6fed356c78d969338a0dff2
nathansolfan <nathansolfan@hotmail.com>
2023-10-27 12:54:12 +0100

    Log in page for the handleSubmit, used as props

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import AuthForm from '../signup/AuthForm'
 
 export default function Login() {
+  // Import the <AuthForm/>
+  // The submission of the form - Create a handler function differently and pass them as props to the form
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
+
   return (
+    // Pass the handleSubmit as prop in the <AuthForm/>
     <main>
       <h2>Login</h2>
+      
+      <AuthForm handleSubmit={handleSubmit}/>
     </main>
   )
 }
---
+++ app/(auth)/signup/AuthForm.jsx
@@ -0,0 +1,38 @@
+// since its not a server component, we have to add this line of code to make it client
+"use client"
+import React, { useState } from 'react'
+
+// Import useState 
+// Get the handleSubmit() prop and use it on the AuthForm()
+export default function AuthForm( {handleSubmit} ) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+  return (
+    // The form itself - label and span - add btn at end
+    // Again (e) is the event from the form submission
+    <form onSubmit={(e) => handleSubmit( e, email, password )}>
+        <label>
+            <span>Email:</span>
+            <input
+            type="text"
+            // the onChange={ has a arrow function}
+            // = () => setEmail()
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            />
+        </label>
+        {/* Password Form */}
+        <label>
+            <span>Password:</span>
+            <input 
+            type="password"
+            onChange={(e)=> setPassword(e.value.target)} 
+            value={password}
+            />
+        </label>
+        {/* When pressed we have access to the Event (e) on the onSubmit() */}
+        <button className='btn-primary'>Submit</button>
+    </form>
+  )
+}
