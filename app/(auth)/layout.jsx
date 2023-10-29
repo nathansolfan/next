80279603bcb6db9efd00d77a39145d086d74ad44
nathansolfan <nathansolfan@hotmail.com>
2023-10-29 12:05:04 +0000

    Protecting Routes and redirect user once loged in

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,8 +1,18 @@
+import {createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import Link from 'next/link'
-import React from 'react'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 // Import the children as props and display it after </nav>
-export default function AuthLayout({children}) {
+export default async function AuthLayout({children}) {
+  // create the Client compo and add the cookie as argument
+  const supabase = createServerComponentClient({cookies})
+  const { data } = await supabase.auth.getSession()
+
+// Here`s the opposite, if user is logd in, redirect to home page
+  if(data.session){
+    redirect('/')
+  }
   return (
     <>
     <nav>
--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 // components
 import Navbar from '../components/Navbar'
 
 // use the special functions - createRouterHandlerClient
 export default async function DashboardLayout({ children }) {
-
 // we back to server component, () to invoke and import at the top
 // need to add the cookies and import
 const supabase = createServerComponentClient( {cookies})
@@ -15,12 +14,17 @@ const supabase = createServerComponentClient( {cookies})
   // pass it as a prop - remember to add await
 const { data } = await supabase.auth.getSession() 
 
+// cant use Router in server components
+if(!data.session){
+  // when user is in the dashboard, but not logged in, Sends it to /login
+  redirect('/login')
+}
+
   return (
     <>
     {/* get it as a prop user={} */}
     <Navbar user={data.session.user }/>
     {children}
-
     </>
   )
 }
